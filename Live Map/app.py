c71e270e4fb51aab056d8c275e6eb5a264696421
Matt Wang <77259515+matthewwangg@users.noreply.github.com>
2024-02-24 20:31:55 -0700

    Updated API key
    Needed step since making repository public, old API Key is now revoked, new API Key is kept privately

--- Live Map/app.py
+++ Live Map/app.py
@@ -1,40 +1,44 @@
 import requests
 import json
-from flask import Flask, render_template, request, jsonify
+import folium
+from folium.plugins import HeatMap
 from PIL import Image
+from IPython.display import display
 import io
-from time import time
+from flask import Flask, render_template, request
+from api_key import API_KEY
 
 app = Flask(__name__, static_url_path='/static')
 
-# Free API Key
-api_key = 'h5OD9nINXtgJrrif15jj2jSlR7xzeBfZ'
+api_key = API_KEY
 
-# Function for gathering the traffic image from MapQuest
 def request_traffic_flow(lat, long):
     url = f'https://www.mapquestapi.com/traffic/v2/flow?key={api_key}&mapLat={lat}&mapLng={long}&mapHeight=400&mapWidth=400&mapScale=433343'
+
     response = requests.get(url)
 
+    # Check if the response content is not empty
     if response.content:
+        # Use PIL to open the image
         traffic_image = Image.open(io.BytesIO(response.content))
         traffic_image = traffic_image.resize((800, 800))
         return traffic_image
     else:
         print("Empty response")
 
-# Function for requesting the map from MapQuest
 def request_respective_map(lat, long):
-    # MapQuest URL
     url = f'https://www.mapquestapi.com/staticmap/v5/map?key={api_key}&center={lat},{long}&zoom=10&size=@2x'
     response = requests.get(url)
 
+    # Check if the response content is not empty
     if response.content:
+        # Use PIL to open the image
         map_image = Image.open(io.BytesIO(response.content))
         return map_image
     else:
         print("Empty response")
+        
 
-# Function for creating the overlapping image
 def create_final_image(traffic_image, map_image):
     mode = 'RGBA'
     traffic_image = traffic_image.convert(mode)
@@ -43,29 +47,26 @@ def create_final_image(traffic_image, map_image):
     final_image = Image.blend(map_image, traffic_image, opacity)
     return final_image
 
-# Route for rendering the index page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+
+    if request.method == 'POST':
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+
+        # Call functions to generate traffic and map images based on user input
+        traffic_image = request_traffic_flow(latitude, longitude)
+        map_image = request_respective_map(latitude, longitude)
+
+        final_image = create_final_image(traffic_image, map_image)
+
+        if final_image:
+            final_image_path = 'static/final_image.png'
+            final_image.save(final_image_path)
+            return render_template('index.html', image_path=final_image_path)
+
+    # Render the initial form
     return render_template('index.html', image_path=None)
-
-# Route for generating the map image
-@app.route('/generate_map', methods=['POST'])
-def generate_map():
-    data = request.json
-    latitude = data['latitude']
-    longitude = data['longitude']
-
-    # Call functions to generate traffic and map images based on user input
-    traffic_image = request_traffic_flow(latitude, longitude)
-    map_image = request_respective_map(latitude, longitude)
-
-    final_image = create_final_image(traffic_image, map_image)
-
-    if final_image:
-        final_image_path = 'static/final_image.png'
-        final_image.save(final_image_path)
-        timestamp = int(time())
-        return jsonify({'image_path': final_image_path, 'timestamp': timestamp})
 
 if __name__ == '__main__':
     app.run(debug=True)
